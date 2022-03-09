3d20b4c8437d416355be8deb7d0d56e765f3fbf6
spider <alvaro_tkd5@hotmail.com>
2022-03-09 20:22:22 -0300

    ultima version

--- backup.py
+++ backup.py
@@ -1,22 +1,14 @@
-from datetime import date
 import shutil
 import os
 
-fecha= date.today()
- 
-fecha_backup = str(fecha).replace('-','-')
 
-origen= r'D:\Alvaro\\escritorio\\Escritorio\\python\bckup\script-backup\\asd'
-destino= r'Z:\\' +'\\' + fecha_backup
+origen= r'D:\\prueba'
+destino= r'D:\\asd' 
 archivos = os.listdir(origen)
-if os.path.isdir(destino):
-    fecha_backup = str(fecha)
-
 for g in archivos:
-        shutil.move(origen + g, destino)
+        shutil.move(origen +'\\'+ g, destino)
     
-    
-
 print(" backup realizado")
+
 
 
